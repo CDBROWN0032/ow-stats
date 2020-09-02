503d0368b12c58add8bb7aeec46e35a9dc18c38b
Christopher Brown <riv4r@live.com>
2020-09-02 05:03:35 -0400

    good db return to view

--- app.js
+++ app.js
@@ -5,25 +5,39 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 44334;
+const port = process.env.PORT || 3000;
 
+// todo: breakout
 const knex = require('knex')({
   client: 'postgres',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'admin',
-    database : 'overwatch'
-  }
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'admin',
+    database: 'overwatch',
+  },
 });
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
 
-app.get('/', (req, res) => {    
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));   
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  const data = await knex
+    .select()
+    .from('games')
+    .join('maps', 'maps.mapid', '=', 'games.MapID')
+    .then((rows) => rows);
+
+  res.render('index', { data });
 });
 
 app.listen(port, () => {
-    debug(`Server running at ${chalk.green("http://localhost:")} ${chalk.green(port)}`);
+  debug(`Server running at ${chalk.green('http://localhost:')} ${chalk.green(port)}`);
 });
